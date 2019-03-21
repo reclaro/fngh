53dc330f1c9fd63a1a759267338eb36f5472400d
Andrea Rosa <andrea.rosa@oracle.com>
2019-03-21 10:07:03 +0000

    version 1 without parameters

--- func.go
+++ func.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	fdk "github.com/fnproject/fdk-go"
 	"github.com/reclaro/golab/ghapi"
@@ -16,22 +15,9 @@ func main() {
 }
 
 func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
-	// you can invoke it passing the repository name via the Fn cli
-	// echo -n '{"repo_name": "fnproject/fn", "query": "TODO"}'| fn invoke oracle-code fngh | jq .
-	sq := &ghapi.SearchQuery{}
-	err := json.NewDecoder(in).Decode(sq)
-	if err != nil && err != io.EOF {
-		_ = json.NewEncoder(out).Encode(&ghapi.SearchResults{Error: fmt.Sprintf("Error in decoding input %s", err.Error())})
-		return
-	}
-	if sq.Repo == "" {
-		sq.Repo = "fnproject/fn"
-	}
-	if sq.Query == "" {
-		sq.Query = "TODO"
-	}
+	sq := &ghapi.SearchQuery{Repo: "fnproject/fn", Query: "TODO"}
 	msg := ghapi.Search(sq)
-	err = json.NewEncoder(out).Encode(&msg)
+	err := json.NewEncoder(out).Encode(&msg)
 	if err != nil {
 		_ = json.NewEncoder(out).Encode(&ghapi.SearchResults{Error: errors.New("Unable to encode results").Error()})
 		return
